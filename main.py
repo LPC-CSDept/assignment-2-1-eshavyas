37bdaa4ac41e79202d84c9c7725ce0ed1b0672f5
eshavyas <vyase58@gmail.com>
2023-02-08 18:26:04 -0800

    hw

--- main.py
+++ main.py
@@ -2,7 +2,23 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    pass
+   #input 
+   numM= int((input())) #input() returns string value. Need to change it to INT value. 
+   # num= int(numM)
+   numF= int(input())
+   
+   #calculation 
+   #total =numM +numF
+   percM = numM / (numM + numF ) *100 
+   percF = numF / (numM+ numF) *100
+
+   #output 
+   print ('Total ', numM + numF)
+   print ('Number of males and females' ,numM, numF)
+  # print ( f ' Percentage of males and females : {percM : 2f} {percF : 2F }) 
+#pass
+
+
 
 
 if __name__ == '__main__':
